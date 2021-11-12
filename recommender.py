e82032f35d229109ccdd377e956770eb3c3b57f4
BenjaminWang236 <ben.lynbrook@gmail.com>
2021-11-12 15:17:14 -0800

    Updated data entry to be Automatic

--- recommender.py
+++ recommender.py
@@ -1,4 +1,4 @@
-import os
+import glob, os
 import numpy as np
 import math
 import pprint as pp
@@ -10,10 +10,14 @@ def main():
     """
     Driver function for the recommender system.
     """
-    datasets = {"train": None, "test5": None, "test10": None, "test20": None}
-    for dataset in datasets:
-        datasets[dataset] = np.loadtxt(f"{dataset}.txt", dtype=int)
-        print(f"{dataset} shape: {datasets[dataset].shape}")
+    datasets = dict()
+    os.chdir(
+        os.path.dirname(os.path.abspath(__file__))
+    )  # Change directory to the directory of this file
+    for file in glob.glob("*.txt"):
+        fileName = file[: -len(".txt")]  # remove .txt from file name
+        datasets[fileName] = np.loadtxt(file, dtype=int)
+        print(f"{fileName} shape:\t{datasets[fileName].shape}")
     pp.pprint(datasets)
 
 
