04d38e9315c55a9fe8f7c8394b8638a2258c00a6
BenjaminWang236 <ben.lynbrook@gmail.com>
2021-11-12 15:05:49 -0800

    Reading in data done

--- recommender.py
+++ recommender.py
@@ -1,1 +1,24 @@
+import os
 import numpy as np
+import math
+import pprint as pp
+from dataclasses import dataclass
+import attr
+
+
+def main():
+    """
+    Driver function for the recommender system.
+    """
+    datasets = {"train": None, "test5": None, "test10": None, "test20": None}
+    for dataset in datasets:
+        datasets[dataset] = np.loadtxt(f"{dataset}.txt", dtype=int)
+        print(f"{dataset} shape: {datasets[dataset].shape}")
+    pp.pprint(datasets)
+
+
+if __name__ == "__main__":
+    """
+    Execute main() if this file is run directly.
+    """
+    main()
